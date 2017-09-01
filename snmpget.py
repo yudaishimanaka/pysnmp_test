a2f1b255831ee9b3c439c3ef74f714a4450ce11b
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-09-01 22:53:55 +0900

    testing pysnmp

---
+++ snmpget.py
@@ -0,0 +1,28 @@
+from pysnmp.hlapi import *
+import threading
+
+def getCpu():
+	for (errorIndication,
+		 errorStatus,
+		 errorIndex,
+		 varBinds) in getCmd(SnmpEngine(),
+							 CommunityData('mysnmp'),
+							 UdpTransportTarget(('192.168.1.1', 161)),
+							 ContextData(),
+							 ObjectType(ObjectIdentity('.1.3.6.1.4.1.2021.11.11.0'))):
+
+		if errorIndication:
+			print(errorIndication)
+			break
+		elif errorStatus:
+			print('%s at %s' % (errorStatus.prettyPrint(),
+								errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+			break
+		else:
+			for varBind in varBinds:
+				print(' = '.join([int(x).prettyPrint() for x in varBind]))
+	t=threading.Timer(1, getCpu)
+	t.start()
+
+t=threading.Thread(target=getCpu)
+t.start()
